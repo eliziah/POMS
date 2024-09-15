2e1c92997532f51047e69517cd7db354f8f24352
Rod Molina <eliziahmolina@gmail.com>
2024-09-15 10:31:16 +0800

    add tooltips and descriptions in creating weekly report

--- resources/views/weekly/weekly-add.blade.php
+++ resources/views/weekly/weekly-add.blade.php
@@ -50,7 +50,7 @@
                     <b>Week Number: </b>{{$week}}
                   </div>
                   <div class="col-lg-12 pb-3 mb-3 border-2 border-bottom ">
-                    <b>Report Scope: </b>From {{$week_friday_start_text}} to {{$week_thursday_end_text}}
+                    <b>Report Scope: </b><u>{{$week_friday_start_text}}</u> to <u>{{$week_thursday_end_text}}</u>
                     <input type="text" name="start" value="{{$week_friday_start}}" hidden>
                     <input type="text" name="end" value="{{$week_thursday_end}}" hidden>
                     <input type="text" name="workweek" value="{{$week}}" hidden>
@@ -61,7 +61,7 @@
                   </div>
                   <div class="col-lg-2 col-md-3">
                     <div class="form-group">
-                      <label for="rag">RAG</label>
+                      <label for="rag">RAG <i class="text-secondary fa-solid fa-question-circle" data-toggle="tooltip" data-placement="top" data-html="true" title="Your subjective assessment of the project's status<br/><br/><b>Green</b> - All is on track<br/><b>Amber</b> - There is a risk<br/><b>Red</b> - Alert, alert, alert!" ></i></label>
                       <select name="rag" id="rag" class="form-control @error('rag') is-invalid @enderror" value="{{$project->rag}}" required>
                         <option value="1" style="color:green">GREEN</option>
                         <option value="2" style="color:orange">AMBER</option>
@@ -74,7 +74,7 @@
                   </div>
                   <div class="col-lg-2 col-md-3">
                     <div class="form-group">
-                      <label for="progress">Progress</label>
+                      <label for="progress">Progress <i class="text-secondary fa-solid fa-question-circle" data-toggle="tooltip" data-placement="top" data-html="true" title="This is based on the progress of your Project Plan this reporting week" ></i></label>
                       <div class="input-group">
                           <input type="number" class="form-control" name="progress" aria-describedby="basic-addon2" value="{{$project->progress}}" >
                           <div class="input-group-append">
@@ -88,7 +88,7 @@
                   </div>
                   <div class="col-lg-3 col-md-3">
                     <div class="form-group">
-                      <label for="phase">Phase</label>
+                      <label for="phase">Phase <i class="text-secondary fa-solid fa-question-circle" data-toggle="tooltip" data-placement="top" data-html="true" title="This is the current phase of your project this reporting week" ></i></label>
                       <select name="phase" id="phase" class="form-control @error('phase') is-invalid @enderror" value="{{$project->phase}}" required>
                         <option value="1">Planning</option>
                         <option value="2">Development</option>
@@ -103,7 +103,7 @@
                   </div>
                   <div class="col-lg-3 col-md-3">
                     <div class="form-group">
-                      <label for="gate">Gate</label>
+                      <label for="gate">Gate  <i class="text-secondary fa-solid fa-question-circle" data-toggle="tooltip" data-placement="top" data-html="true" title="This is the current gate of your project this reporting week" ></i></label>
                       <select name="gate" id="gate" class="form-control @error('gate') is-invalid @enderror" value="{{$project->gate}}" required>
                         <option value="1">G1-Initiation</option>
                         <option value="2">G2-Planning</option>
@@ -118,8 +118,8 @@
                   </div>
                   <div class="col-lg-12">
                     <div class="form-group">
-                        <label for="executive">Executive Summary</label>
-                        <textarea name="executive" id="executive" minlength="150" maxlength="255" class="form-control @error('executive') is-invalid @enderror" rows="2">{{old('executive')}}</textarea>
+                        <label for="executive">Executive Summary  <i class="text-secondary fa-solid fa-question-circle" data-toggle="tooltip" data-placement="top" data-html="true" title="A summary of your project update for this reporting week. An executive must be able to understand all your updates by reading this executive update alone.<br/><br/>Minimum of 150 Characters;<br/>Maximum of 255 Characters" ></i></label>
+                        <textarea name="executive" placeholder="This is required. All on-going project must have an executive update." id="executive" minlength="150" maxlength="255" class="form-control @error('executive') is-invalid @enderror" rows="2">{{old('executive')}}</textarea>
                         @error('executive')
                         <span class="invalid-feedback text-danger">{{ $message }}</span>
                         @enderror
@@ -128,7 +128,7 @@
                   <div class="col-lg-6">
                     <div class="form-group">
                         <label for="highlights">Project Highlights</label>
-                        <textarea name="highlights" id="highlights" maxlength="1000" class="form-control @error('highlights') is-invalid @enderror" rows="4">{{old('highlights')}}</textarea>
+                        <textarea name="highlights" placeholder="Leave it blank if no entry." id="highlights" maxlength="1000" class="form-control @error('highlights') is-invalid @enderror" rows="4">{{old('highlights')}}</textarea>
                         @error('highlights')
                         <span class="invalid-feedback text-danger">{{ $message }}</span>
                         @enderror
@@ -137,7 +137,7 @@
                   <div class="col-lg-6">
                     <div class="form-group">
                         <label for="risks">Project Risks</label>
-                        <textarea name="risks" id="risks" maxlength="1000" class="form-control @error('risks') is-invalid @enderror" rows="4">{{old('risks')}}</textarea>
+                        <textarea name="risks" placeholder="Leave it blank if no entry." id="risks" maxlength="1000" class="form-control @error('risks') is-invalid @enderror" rows="4">{{old('risks')}}</textarea>
                         @error('risks')
                         <span class="invalid-feedback text-danger">{{ $message }}</span>
                         @enderror
@@ -146,7 +146,7 @@
                   <div class="col-lg-6">
                     <div class="form-group">
                         <label for="nextsteps">Project Next Steps</label>
-                        <textarea name="nextsteps" id="nextsteps" maxlength="1000" class="form-control @error('nextsteps') is-invalid @enderror" rows="4">{{old('nextsteps')}}</textarea>
+                        <textarea name="nextsteps" placeholder="Leave it blank if no entry." id="nextsteps" maxlength="1000" class="form-control @error('nextsteps') is-invalid @enderror" rows="4">{{old('nextsteps')}}</textarea>
                         @error('nextsteps')
                         <span class="invalid-feedback text-danger">{{ $message }}</span>
                         @enderror
@@ -155,7 +155,7 @@
                   <div class="col-lg-6">
                     <div class="form-group">
                         <label for="help">Project Help Needed</label>
-                        <textarea name="help" id="help" maxlength="1000" class="form-control @error('help') is-invalid @enderror" rows="4">{{old('help')}}</textarea>
+                        <textarea name="help" placeholder="Leave it blank if no entry." id="help" maxlength="1000" class="form-control @error('help') is-invalid @enderror" rows="4">{{old('help')}}</textarea>
                         @error('help')
                         <span class="invalid-feedback text-danger">{{ $message }}</span>
                         @enderror
