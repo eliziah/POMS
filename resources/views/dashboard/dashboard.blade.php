5bc1302e30e987351710ab8cd0758c7575d6c289
Rod Molina <eliziahmolina@gmail.com>
2024-10-03 08:24:14 +0800

    external latest updates in dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -384,7 +384,7 @@
                     <div class="col-xl-12">
                         <div class="card shadow mb-4 w-100">
                             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-primary">Internal Latest Updates</h6>
+                                <h6 class="m-0 font-weight-bold text-primary">External Latest Updates</h6>
                             </div>
                             <!-- Card Body -->
                             <div class="card-body">
